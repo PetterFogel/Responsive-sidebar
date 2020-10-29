72f1239b3670ef2bfe6cb361ea707b31a00170cf
unknown <petter.fogel@medieinstitutet.se>
2020-10-29 14:44:18 +0100

    added exit icon and styled it and logic

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let sideBar = document.querySelector(".sidebar");
 let burgerButton = document.querySelector(".burger");
+let exitIcon = document.querySelector(".exit");
 
 burgerButton.onclick = function() {
-    sideBar.classList.toggle("sidebar-active");
+    sideBar.classList.add("sidebar-active");
+}
+
+exitIcon.onclick = function() {
+    sideBar.classList.remove("sidebar-active");
 }
